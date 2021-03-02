1482ff92bd8e6efc18dda5bed11b402972cfdc26
Felipe Andrade <felipe.andrade0918@gmail.com>
2021-03-02 19:18:41 -0300

    [feat] Add AgencyService tests

--- src/test/java/com/bank/agencies/fixture/AgenciesFixture.java
+++ src/test/java/com/bank/agencies/fixture/AgenciesFixture.java
@@ -6,7 +6,15 @@ import java.util.List;
 
 public class AgenciesFixture {
 
-    public static List<AgencyGatewayResponse> gimmeBasicAgencies() {
-        return List.of(AgencyGatewayResponse.AgencyGatewayResponseBuilder.anAgency().bank("BANCO DO BRASIL S.A.").name("EMPRESA JOAO PESSOA").city("JOAO PESSOA").state("PB").build());
-    }
+	public static List<AgencyGatewayResponse> gimmeBasicAgencies() {
+		return List.of(
+				AgencyGatewayResponse.AgencyGatewayResponseBuilder.anAgency().bank("BANCO DO BRASIL S.A.")
+				.name("EMPRESA JOAO PESSOA").city("JOAO PESSOA").state("AC").build(),
+				AgencyGatewayResponse.AgencyGatewayResponseBuilder.anAgency().bank("BANCO DO BRASIL S.A.")
+				.name("EMPRESA BAURU").city("BAURU").state("SP").build(),
+				AgencyGatewayResponse.AgencyGatewayResponseBuilder.anAgency().bank("BANCO DO BRASIL S.A.")
+						.name("EMPRESA SAO JOSE DOS CAMPOS").city("SAO JOSE DOS CAMPOS").state("SP").build()
+				)
+				;
+	}
 }
--- src/test/java/com/bank/agencies/service/AgencyServiceTest.java
+++ src/test/java/com/bank/agencies/service/AgencyServiceTest.java
@@ -1,5 +1,6 @@
 package com.bank.agencies.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -7,33 +8,64 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.bank.agencies.domain.AgencyGatewayResponse;
+import com.bank.agencies.domain.AgencyResponse;
 import com.bank.agencies.external.gateway.AgenciesGateway;
 import com.bank.agencies.fixture.AgenciesFixture;
 
 public class AgencyServiceTest {
 
     final AgenciesGateway gateway = mock(AgenciesGateway.class);
-    final AgencyService findAllAgencies = new AgencyService(gateway);
+    
+    AgencyService agencyService;
 
     List<AgencyGatewayResponse> agencies;
+    
+    @BeforeEach
+    public void setup() {
+    	agencyService = new AgencyService(gateway);
+    }
 
     @Test
     void shouldExecuteUseCaseFindAllBBAgenciesWithSuccess() {
         // given
         agencies = AgenciesFixture.gimmeBasicAgencies();
-        when(findAllAgencies.execute()).thenReturn(agencies);
+        when(agencyService.execute()).thenReturn(agencies);
 
         // when
-        final List<AgencyGatewayResponse> response = findAllAgencies.execute();
+        final List<AgencyGatewayResponse> response = agencyService.execute();
 
         // then
         verify(gateway, times(1)).findAllAgencies();
 
         assertNotNull(response);
+        assertEquals(3, response.size());
         assertNotNull(response.get(0));
+    }
+    
+    @Test
+    void shouldListAllTheAgenciesAndGroupThemByState() {
+    	// given
+        agencies = AgenciesFixture.gimmeBasicAgencies();
+        when(gateway.findAllAgenciesSortedByStateAndCity()).thenReturn(agencies);
+
+        // when
+        Map<String, List<AgencyResponse>> agenciesGroupedByState = agencyService.findAllAgenciesGroupedByState();
+
+        // then
+        verify(gateway, times(1)).findAllAgenciesSortedByStateAndCity();
+
+        assertNotNull(agenciesGroupedByState);
+        assertEquals(2, agenciesGroupedByState.size());
+        assertEquals(1, agenciesGroupedByState.get("AC").size());
+        assertEquals("JOAO PESSOA", agenciesGroupedByState.get("AC").get(0).getCity());
+        assertEquals("BAURU", agenciesGroupedByState.get("SP").get(0).getCity());
+        assertEquals("SAO JOSE DOS CAMPOS", agenciesGroupedByState.get("SP").get(1).getCity());
+        assertEquals(2, agenciesGroupedByState.get("SP").size());
     }
 }
