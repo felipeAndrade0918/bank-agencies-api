b4d79a0c0f9312ac9d280cee48f8a7f1d58aaedc
Felipe Andrade <felipe.andrade0918@gmail.com>
2021-03-02 18:34:05 -0300

    [feat] Add method to list agencies sorted by state and city

--- src/main/java/com/bank/agencies/external/api/AgenciesGatewayImpl.java
+++ src/main/java/com/bank/agencies/external/api/AgenciesGatewayImpl.java
@@ -1,14 +1,5 @@
 package com.bank.agencies.external.api;
 
-import com.bank.agencies.external.gateway.AgenciesGateway;
-import com.bank.agencies.domain.AgencyGatewayResponse;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,8 +7,17 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.bank.agencies.domain.AgencyGatewayResponse;
+import com.bank.agencies.external.gateway.AgenciesGateway;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class AgenciesGatewayImpl implements AgenciesGateway {
@@ -34,29 +34,58 @@ public class AgenciesGatewayImpl implements AgenciesGateway {
 
     @Override
     public List<AgencyGatewayResponse> findAllAgencies() {
+        URI apiURI = getBaseParams().build().toUri();
 
-        URI apiURI = UriComponentsBuilder
-                .fromUriString(baseUrl)
-                .queryParam("$format", "json")
-                .build().toUri();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(apiURI)
-                .build();
+        HttpRequest request = buildRequest(apiURI);
 
         try {
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-            if (response.statusCode() == HttpStatus.OK.value()) {
-                JsonNode parent = mapper.readTree(response.body());
-                String content = parent.get("value").toString();
-                return Arrays.asList(mapper.readValue(content, AgencyGatewayResponse[].class));
-            }
+            String responseContent = getResponseContent(response);
+            return Arrays.asList(mapper.readValue(responseContent, AgencyGatewayResponse[].class));
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Error when trying get all Agencies from API");
+            throw new RuntimeException("Error when trying get all Agencies from the API");
         }
-
-        return Collections.emptyList();
     }
+
+	@Override
+	public List<AgencyGatewayResponse> findAllAgenciesSortedByStateAndCity() {
+		URI apiURI = getBaseParams()
+				.queryParam("$orderby", "UF asc,Municipio asc")
+				.build()
+				.toUri();
+
+        HttpRequest request = buildRequest(apiURI);
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String responseContent = getResponseContent(response);
+            return Arrays.asList(mapper.readValue(responseContent, AgencyGatewayResponse[].class));
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Error when trying get all Agencies grouped by state from the API");
+        }
+	}
+
+	private HttpRequest buildRequest(URI apiURI) {
+		HttpRequest request = HttpRequest.newBuilder()
+        		.GET()
+        		.uri(apiURI)
+        		.build();
+		return request;
+	}
+
+	private UriComponentsBuilder getBaseParams() {
+		return UriComponentsBuilder.fromUriString(baseUrl)
+                .queryParam("$format", "json");
+	}
+	
+	private String getResponseContent(HttpResponse<String> response) throws IOException {
+		if (response.statusCode() == HttpStatus.OK.value()) {
+            JsonNode parent = mapper.readTree(response.body());
+            return parent.get("value").toString();
+        }
+		
+		throw new IllegalStateException("Response did not return 200");
+	}
 }
